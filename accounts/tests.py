4fe4be48aa15b52f62bc1d167c038747dbdc4085
humanbeing-dev <maciej.sitowski@gmail.com>
2021-02-13 10:08:25 +0100

    Add tests to CustomUser
    Test username, email, goal

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class TestCustomUser(TestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='testuser',
+            email='testuser@wp.pl',
+            password='password',
+            goal='To be'
+        )
+
+    def test_custom_user_username(self):
+        user = get_user_model().objects.get(id=1)
+        self.assertEqual(user.username, 'testuser')
+    
+    def test_custom_user_email(self):
+        user = get_user_model().objects.get(id=1)
+        self.assertEqual(user.email, 'testuser@wp.pl')
+    
+    def test_custom_user_goal(self):
+        user = get_user_model().objects.get(id=1)
+        self.assertEqual(user.goal, 'To be')
