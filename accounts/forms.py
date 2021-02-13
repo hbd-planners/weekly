8f63180087b744f92d79236beac4fffc7335f6d8
humanbeing-dev <maciej.sitowski@gmail.com>
2021-02-13 09:43:22 +0100

    Create Custom User Model
    CustomUser model was created
    Forms was modified for CustomUser
    Admin panel was modified in admin.py

---
+++ accounts/forms.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django import forms
+from .models import CustomUser
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = UserCreationForm.Meta.fields + ('goal',)
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = CustomUser
+        fields = UserChangeForm.Meta.fields
+
