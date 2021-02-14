6ced09cc5ea7a8146652d83994450613ec122022
humanbeing-dev <maciej.sitowski@gmail.com>
2021-02-14 16:04:08 +0100

    Set up email settings

--- config/settings.py
+++ config/settings.py
@@ -139,5 +139,11 @@ LOGOUT_REDIRECT_URL = 'home'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL SETTINGS
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+DEFAULT_FROM_EMAIL = env.str('DEFAULT_FROM_EMAIL') 
+EMAIL_HOST = env.str('EMAIL_HOST') 
+EMAIL_HOST_USER = env.str('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env.str('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
